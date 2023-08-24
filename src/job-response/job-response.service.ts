27edbd25b8460ad0432681985c01849ce525dfe8
Vladimir Staninov <52317262+cantona94@users.noreply.github.com>
2023-08-24 19:17:21 +0500

    add check existence jobResponse

--- src/job-response/job-response.service.ts
+++ src/job-response/job-response.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { CreateJobResponseDto } from './dto/create-job-response.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { JobResponse } from './entities/job-response.entity';
@@ -12,6 +12,14 @@ export class JobResponseService {
   ) {}
 
   async create(createJobResponseDto: CreateJobResponseDto) {
+    const existJobResponseDto = await this.jobResponseRepository.findOne({
+      where: {
+        user: { id: +createJobResponseDto.user},
+        vacancy: { id: +createJobResponseDto.vacancy},
+      },
+    });
+    if (existJobResponseDto) throw new BadRequestException('This job response already exist');
+
     const newJobResponse = {
       user: { id: +createJobResponseDto.user },
       vacancy: { id: +createJobResponseDto.vacancy },
