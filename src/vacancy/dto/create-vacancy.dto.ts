54bf5854a38bdb294ca484ab7c4c4015b628f6cd
Vladimir Staninov <52317262+cantona94@users.noreply.github.com>
2023-08-24 09:50:30 +0500

    refactor code

--- src/vacancy/dto/create-vacancy.dto.ts
+++ src/vacancy/dto/create-vacancy.dto.ts
@@ -1,17 +1,27 @@
-import { IsNotEmpty, MaxLength } from 'class-validator';
+import {
+  ArrayMinSize,
+  IsArray,
+  IsNotEmpty,
+  IsString,
+  MaxLength,
+} from 'class-validator';
 import { User } from 'src/user/entities/user.entity';
 
 export class CreateVacancyDto {
   @IsNotEmpty()
+  @IsString()
   name: string;
 
   @IsNotEmpty()
   user: User;
 
   @IsNotEmpty()
+  @IsString()
   @MaxLength(300, { message: 'Description must be no more than 300 symbols' })
   description: string;
 
-  @IsNotEmpty()
+  @IsArray()
+  @IsString({ each: true })
+  @ArrayMinSize(1)
   arraySkills: string[];
 }
